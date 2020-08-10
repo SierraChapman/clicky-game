049e639ab4964904e6e9d5e38364490c86381076
Sierra Chapman <siechap@gmail.com>
2020-08-10 16:54:01 -0700

    add initial Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Clicky Game</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
