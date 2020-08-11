71ace72be816f94c53a8028045e572b2e0a4bed1
Sierra Chapman <siechap@gmail.com>
2020-08-11 01:44:37 -0700

    Updates

---
+++ precache-manifest.398fa280475d0e872696ecb5db558203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e129c5771c49674bc76e6d04e17d2f5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "880f2a07f8a9ce609c6c",
+    "url": "/clicky-game/static/css/main.8536836f.chunk.css"
+  },
+  {
+    "revision": "44e3eb19e8328bef502d",
+    "url": "/clicky-game/static/js/2.f228d9b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.f228d9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880f2a07f8a9ce609c6c",
+    "url": "/clicky-game/static/js/main.e126fa8d.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
