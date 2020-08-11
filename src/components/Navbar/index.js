37f8032a43285b7a58e71c04ef167c80f4e5a820
Sierra Chapman <siechap@gmail.com>
2020-08-10 17:30:42 -0700

    style Navbar for small screens

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,9 +4,14 @@ import './style.css';
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h1>Clicky Game</h1>
-      </div>
+      <header className="container-fluid fixed-top ">
+        <div className="row">
+          <div className="col-12"><h1 className="mt-2">Clicky Game</h1></div>
+          <div className="col-12"><p className="mb-1">with images from</p></div>
+          <div className="col-12"><p id="hst"><strong>HUBBLE</strong> SPACE TELESCOPE</p></div>
+          <div className="col-12"><p>Score: 0 | Top Score: 12</p></div>
+        </div>
+      </header>
     );
   }
 }
