57997e5d1190b98aa937d7edccccd8c8eea6395f
Sierra Chapman <siechap@gmail.com>
2020-08-10 23:42:36 -0700

    create jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Jumbotron />
       </div>
     );
   }
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Jumbotron extends Component {
+  render() {
+    return (
+      <div id="jumbotron" className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h2 className="display-4">Welcome to the Clicky Game!</h2>
+          <p className="lead">Click on images to earn points, but don't click on the same image twice!</p>
+        </div>
+        <div className="img-credit"> Image credit: NASA, ESA, and S. Beckwith (STScI) and the HUDF Team</div>
+      </div>
+    );
+  }
+}
+
+export default Jumbotron;
