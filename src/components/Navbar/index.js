a7adb0e4c6fdecd75ffb0d2d9a96fadcc18febcf
Sierra Chapman <siechap@gmail.com>
2020-08-10 22:48:38 -0700

    style navbar for larger screens

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,10 +6,10 @@ class Navbar extends Component {
     return (
       <header className="container-fluid fixed-top ">
         <div className="row">
-          <div className="col-12"><h1 className="mt-2">Clicky Game</h1></div>
-          <div className="col-12"><p className="mb-1">with images from</p></div>
-          <div className="col-12"><p id="hst"><strong>HUBBLE</strong> SPACE TELESCOPE</p></div>
-          <div className="col-12"><p>Score: 0 | Top Score: 12</p></div>
+          <div className="col-12 col-md-auto"><h1 className="mt-2">Clicky Game</h1></div>
+          <div className="col-12 col-md-auto"><p id="small-title-text" className="mb-1">with images from</p></div>
+          <div className="col-12 col-md-auto"><p id="hst"><strong>HUBBLE</strong> SPACE TELESCOPE</p></div>
+          <div id="score-div" className="col-12 col-lg"><p>Score: 0 | Top Score: 12</p></div>
         </div>
       </header>
     );
