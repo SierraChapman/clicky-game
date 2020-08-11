31608278c1cf2cf6eb11708778732ff4bd629326
Sierra Chapman <siechap@gmail.com>
2020-08-11 11:18:21 -0700

    add PUBLIC_URL to path for jumbotron image

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ import './style.css';
 class Jumbotron extends Component {
   render() {
     return (
-      <div id="jumbotron" className="jumbotron jumbotron-fluid">
+      <div id="jumbotron" className="jumbotron jumbotron-fluid" style={{ backgroundImage: `url("${process.env.PUBLIC_URL}/img/hubble-deep-field.jpg")` }}>
         <div className="container">
           <h2 className="display-4">Welcome to the Clicky Game!</h2>
           <p className="lead">Click on images to earn points, but don't click on the same image twice!</p>
